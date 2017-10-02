23a3e294164029c14d6cf2dcc9db521c8a228206
Neddy <6915421+neddy70@users.noreply.github.com>
2017-10-02 18:30:50 +1100

    Chapter 10 Update

--- 10. Exercises/14_ex.rb
+++ 10. Exercises/14_ex.rb
@@ -7,18 +7,3 @@ contact_data.each_index do |i|
   contacts["Joe Smith"][labels[i]] = contact_data[i]
 end
 puts contacts
-
-
-contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
-            ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
-contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
-labels = [:email, :address, :phone]
-n = 0
-contacts.each do |contact|
-  puts contact[0]
-  labels.each_index do |i|
-    contacts[contact[0]][labels[i]] = contact_data[n[0]]
-  end
-  n += 1
-end
-puts contacts
